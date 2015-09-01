947c3fe2a7db65bbb9277e97f39f4f15b1a95e69
jaber <hiqfs@qq.com>
2015-09-01 15:30:59 +0800

    New start

---
+++ README.md
@@ -0,0 +1,6 @@
+jaber Stie
+=============
+a web Stie
+-----------
+### link
+[hifs.tk](http://hifs.tk)<br />
---
+++ js/background.js
@@ -0,0 +1,9 @@
+htmlobj=$.ajax({url:"http://server-php.coding.io/imgaddress.php",async:true});
+var address=htmlobj.responseText;
+var arr = address.split("<br>");
+var index = Math.floor(Math.random() * arr.length);
+index-=1;
+if (index==-1) {
+  index+=1;
+}
+document.write('<style>body{background-image:url(' + arr[index] + ')}</style>');
