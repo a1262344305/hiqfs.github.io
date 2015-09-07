cc23c913b1bde8d36b838529431d9032462428e0
jaber <i@hifs.tk>
2015-09-07 22:53:11 +0800

    平滑滚动,footer code 格式化

--- js/init.js
+++ js/init.js
@@ -6,7 +6,25 @@ var disqus_shortname = 'jaberz';
     dsq.src = "/js/disqus_embed.js";
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 })();
-document.getElementById("footer").innerHTML="<div class="+"footer"+"><a href="+"http://hifs.tk"+">hifs.tk"+" "+"</a><a href="+"mailto:i@hifs.tk"+">联系我</a> <a href="+"http://hifs.tk/About.htm"+">关于我</a><p style='color: darkslategray; font-family: "+"clicker script"+","+"segoe script"+","+"Comic Sans MS"+"; font-size: 28px; margin-top: 1px; text-shadow: none;'>"+"life,love,linux"+"</p></div>";
+$(document).ready(function() {
+       $("html").niceScroll();
+});
+$(document).ready(function() {
+    document.getElementById("footer").innerHTML="<div class="
+      +"footer"
+      +"><a href="
+      +"http://hifs.tk"
+      +">hifs.tk"
+      +" "
+      +"</a><a href="
+      +"mailto:i@hifs.tk"
+      +">联系我</a> <a href="
+      +"http://hifs.tk/About.htm"
+      +">关于我</a><p>"
+      +"life,love,linux"
+      +"</p></div>";
+});
+//document.getElementById("footer").innerHTML="<div class="+"footer"+"><a href="+"http://hifs.tk"+">hifs.tk"+" "+"</a><a href="+"mailto:i@hifs.tk"+">联系我</a> <a href="+"http://hifs.tk/About.htm"+">关于我</a><p style='color: darkslategray; font-family: "+"clicker script"+","+"segoe script"+","+"Comic Sans MS"+"; font-size: 28px; margin-top: 1px; text-shadow: none;'>"+"life,love,linux"+"</p></div>";
 /* htmlobj=$.ajax({url:"http://server-php.coding.io/imgaddress.php",async:true});
 var address=htmlobj.responseText;
 var arr = address.split("<br>");
