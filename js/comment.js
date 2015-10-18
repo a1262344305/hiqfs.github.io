b33b78fdc239d32500bf9aafae554a438053498f
jaber <i@hifs.tk>
2015-10-18 15:40:23 +0800

    comment

--- js/comment.js
+++ js/comment.js
@@ -1,28 +1,34 @@
-
-function comment(){
-  comment=$.ajax({url:"http://server-php.coding.io/read.php",async:false});
-  var com = comment.responseText;
-  var commit = com.split("|");
-  $(document).ready(function() {
-    document.getElementById("commit").innerHTML=commit;
-  });
-}
-comment();
-function tijiaopost() {
-  tijiaopost = $.ajax({
-    url:"http://server-php.coding.io/write.php",
-    type:"post",
-    dataType:"text",
-    data:{comment:document.getElementById("ti").value},
-    success:function(data) {
-      alert("success");
-      comment();
-    },
-    error:function (data) {
-      alert("error");
-    },
-  });
-}
+$(document).ready(function() {
+comment=$.ajax({url:"http://server-php.coding.io/read.php",async:false});
+var com = comment.responseText;
+var commit = com.split("|");
+document.getElementById("commit").innerHTML=commit;
+});
+  function tijiaopost() {
+    tijiaopostand = $.ajax({
+      url:"http://server-php.coding.io/write.php",
+      type:"post",
+      data:{comment:document.getElementById("ti").value},
+      success:function(data,textStatus) {
+        alert("success");
+        $('#commit').empty()
+        $('#commit').html(function(){
+          comment=$.ajax({url:"http://127.0.0.1:8080/read.php",async:false});
+          var com = comment.responseText;
+          var commit = com.split("|");
+          return commit;
+        });
+      },
+      error:function (XMLHttpRequest, textStatus, errorThrown) {
+        console.log(XMLHttpRequest);
+        console.log(textStatus);
+        console.log(errorThrown);
+        alert("error");
+      },
+      async:"true",
+      cache:"false"
+    });
+  }
 /*
 $("#comment").ajaxSubmit({
                     type: 'post',
