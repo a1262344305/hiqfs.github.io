edaf406f20ff6c35766e1cdc979bd199b4c0d8b8
jaber <i@hifs.tk>
2015-11-09 22:20:52 +0800

    2015年 11月 09日 星期一 22:20:52 CST

--- mobile/js/init.js
+++ mobile/js/init.js
@@ -1,15 +1,4 @@
+text="<div class=\"footer\"><p>life,love,linux</p></div>";
 $(document).ready(function() {
-    document.getElementById("footer").innerHTML="<div class="
-      +"footer"
-      +"><a href="
-      +"http://hifs.tk"
-      +">hifs.tk"
-      +" "
-      +"</a><a href="
-      +"mailto:i@hifs.tk"
-      +">联系我</a> <a href="
-      +"http://hifs.tk/About.htm"
-      +">关于我</a><p>"
-      +"life,love,linux"
-      +"</p></div>";
+    document.getElementById("footer").innerHTML=text;
 });
