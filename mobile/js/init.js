aff4b01201dc54297ef78cd8486977c34a942883
lynd <i@hifs.tk>
2015-11-19 19:22:14 +0800

    bug

--- mobile/js/init.js
+++ mobile/js/init.js
@@ -15,7 +15,6 @@ function hius() {
     document.body.appendChild(hjs);
   },100);
 }
-hius();
 function mobile() {
     if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
     }
