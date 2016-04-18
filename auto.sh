bb4cdeb893c1ada0b91b3154b8d6e6977630dea6
佳冰 <hiqfs@qq.com>
2016-04-18 13:31:23 +0800

    Mon Apr 18 13:31:23     2016

--- auto.sh
+++ auto.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+#!/bin/sh
+time=$(date)
+echo $time > ./text/version.txt
 git add --all .
-git commit -m "$(date) $1"
+git commit -m "$time $1"
 git push github master
 git push origin master
 #自动部署代码
