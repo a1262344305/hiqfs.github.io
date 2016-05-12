43a8e4d2ba88264eaf9c8608c9e36b5faf2f8c2e
佳冰 <hiqfs@qq.com>
2016-05-12 16:31:50 +0800

    Thu May 12 16:31:50     2016 bug修复

--- auto.sh
+++ auto.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-#!/bin/sh
 time=$(date)
+#开始处理代码
+sed -i "s/\/static/http:\/\/7xsbbu.com1.z0.glb.clouddn.com\/static/g" ./*.html #使用七牛cdn减速服务
 echo "$time $1" > ./static/text/version.txt
 git add --all .
 git commit -m "$time $1"
 git pull
-sed -i "s/\/static/http:\/\/7xsbbu.com1.z0.glb.clouddn.com\/static/g" index.html #使用七牛cdn减速服务
 git push github master
 git push origin master
-git push gitlab master 
-sed -i "s/http:\/\/7xsbbu.com1.z0.glb.clouddn.com\/static/\/static/g" index.html #变为dev状态
+git push gitlab master
+sed -i "s/http:\/\/7xsbbu.com1.z0.glb.clouddn.com\/static/\/static/g" ./*.html
 #自动部署代码
